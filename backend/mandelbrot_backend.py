c5a1d22c96d54dec555f017b45422d16d6fb8b6a
Luc Choubert <luc.choubert@gmail.com>
2022-11-15 22:38:05 +0100

    Cleaner version

--- backend/mandelbrot_backend.py
+++ backend/mandelbrot_backend.py
@@ -3,49 +3,42 @@
 
 import json
 import io
-import random
 from PIL import Image
-from flask import Flask, request, jsonify, send_file, url_for
+from flask import Flask, request, jsonify, send_file
 from MandelbrotPoint import MandelbrotPoint, MandelbrotSet
 
 app = Flask(__name__)
 
 
-#ReDo it in a better way
-# Doc here: https://flask.palletsprojects.com/en/2.2.x/patterns/favicon/
+# Return iconfile
 @app.get('/favicon.ico')
 def get_ico():
     filename = 'favicon.ico'
     return send_file(filename, mimetype='image/vnd.microsoft.icon')
 
-@app.post('/image')
-def get_image():
-    #record = json.loads(request.data)
-    filename = 'mandelbrot.jpeg'
-    return send_file(filename, mimetype='image/jpeg')
-
+# Test endpoint to quickly check that backend is up and running
 @app.post('/test')
 def test():
-    #record = json.loads(request.data)
-    return jsonify({'name': 'alice',
-                    'email': 'alice@outlook.com'})
+    return jsonify({'status': 'up'})
 
+# Endpoint to draw the Mandelbrot Set fractale
 @app.post('/draw')
 def draw_image():
+    # Get & decode the input json
     record = json.loads(request.data)
-    mode = "RGB"
-    buf = io.BytesIO()
-#    myImage = Image.new(mode, (640, 480), color=0)
-#    for i in range(myImage.size[0]):  # for every pixel:
-#        for j in range(myImage.size[1]):
-#            myImage.putpixel((i, j), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+
+    # Build the Mandelbrot Set
     mSet = MandelbrotSet(float(record['xmin']),
                          float(record['xmax']),
                          float(record['ymin']),
                          float(record['ymax']))
     mSet.compute()
+
+    # Build reply as a jpeg image
     myImage = mSet.exportToImage()
+    buf = io.BytesIO()
     myImage.save(buf, format='JPEG')
+
     return buf.getvalue(), {"Content-Type": "image/jpeg"}
 
 if __name__ == '__main__':
